876767f07ce9df3c76c6e7525474b48926842ffa
chen.chongyang <22377832@qq.com>
2019-11-04 10:31:09 +0800

    Python 之禅，import this。 python2 整除 （3/2）= 1。 42 is my favorite number

--- chapter2/birthday.py
+++ chapter2/birthday.py
@@ -2,4 +2,15 @@ age = 23;
 message = "Happy " + str(age) + "rd BirthDay!"
 print(message)
 
+# 练习
 
+print(4 + 4)
+print(8 / 1)
+print(8 % 9)
+print(2 ** 3)
+print(8 - 0)
+print(2 * 4)
+
+number = 42;
+message = "My Favorite number is " + str(number) + "."
+print(message)
--- chapter2/number.py
+++ chapter2/number.py
@@ -6,5 +6,7 @@ print(3 ** 2)
 print(2 + 3 * 4)
 print((2 + 3) * 4)
 
+# python 2 整数
+print(3/2)  # 1
 
 
