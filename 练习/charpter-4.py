cc3a6ff19d4442e12dc64d58e150e7b83a905660
chen.chongyang <22377832@qq.com>
2019-11-04 13:53:16 +0800

    数值列表。

---
+++ 练习/charpter-4.py
@@ -0,0 +1,12 @@
+# 4-1
+pizzas = ['茄子', '辣椒', '土豆']
+for pizza in pizzas:
+    print("I like %s pizza." % pizza)
+print("I really like pizza!")
+
+# 4-2
+pets = ["dog", 'cat', 'pig']
+for pet in pets:
+    print("A {} would make a great pet.".format(pet))
+print("Any of these animals would make a great pet!")
+
