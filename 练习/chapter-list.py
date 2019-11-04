3d0044bb762eaa18d7bf22187ec5aa3e65d283a2
chen.chongyang <22377832@qq.com>
2019-11-04 10:48:12 +0800

    添加练习目录， 列表list

---
+++ 练习/chapter-list.py
@@ -0,0 +1,13 @@
+# 3-1
+names = ["xiao ming", "xiao hong", "han meimei"]
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+# 3-2
+print("Hello, " + names[0].title() + "!")
+print("Hello, " + names[1].title() + "!")
+print("Hello, " + names[2].title() + "!")
+
+# 3-3
+
