21d3370dca55ebdf96f914474acc645577faab3f
chen.chongyang <22377832@qq.com>
2019-11-05 15:20:39 +0800

    追加内容到文件。

--- chapter10/write_message.py
+++ chapter10/write_message.py
@@ -1,3 +1,4 @@
 filename = 'programming.txt'
-with open(filename, 'w') as f:
-    f.write('I love programming.')
+with open(filename, 'a') as f:
+    f.write('I also love finding meaning in large datasets.\n')
+    f.write('I love creating apps that can run in a browser.\n')
